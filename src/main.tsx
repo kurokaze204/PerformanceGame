3f66330c0ecaf0a40caf312c2403038457d86477
Stuart French <6202229+kurokaze204@users.noreply.github.com>
2026-08-29 01:30:35 +1000

    ui: run board v2 game flow

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import AppBoard from './AppBoard.tsx';
+import AppBoardV2 from './AppBoardV2.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppBoard />
+    <AppBoardV2 />
   </StrictMode>,
 );
