11fd8ac399729ac9ac87a9a30c356875c10548af
Stuart French <6202229+kurokaze204@users.noreply.github.com>
2026-08-30 00:26:48 +1000

    Load Invest panel layout override

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import AppBoardV6 from './AppBoardV6.tsx';
 import { GlobalRiverTool } from './components/GlobalRiverTool.tsx';
 import './index.css';
+import './invest-panel-layout.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
