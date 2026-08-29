68c525fff87d7fe7245382f26c216899c4133066
Stuart French <6202229+kurokaze204@users.noreply.github.com>
2026-08-29 12:47:12 +1000

    Mount global river diagram tool

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppBoardV6 from './AppBoardV6.tsx';
+import { GlobalRiverTool } from './components/GlobalRiverTool.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppBoardV6 />
+    <GlobalRiverTool />
   </StrictMode>,
 );
