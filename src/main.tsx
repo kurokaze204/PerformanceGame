129733b0ae1c51881801008c25ff5f41f558e428
Stuart French <6202229+kurokaze204@users.noreply.github.com>
2026-09-03 21:53:29 +1000

    Mount global click feedback and responsive layout guard

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppBoardV6 from './AppBoardV6.tsx';
-import { GlobalRiverTool } from './components/GlobalRiverTool.tsx';
+import { NetworkActionFeedback } from './components/NetworkActionFeedback.tsx';
+import { ViewportLayoutGuard } from './components/ViewportLayoutGuard.tsx';
 import './index.css';
 import './invest-panel-layout.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ViewportLayoutGuard />
+    <NetworkActionFeedback />
     <AppBoardV6 />
-    <GlobalRiverTool />
   </StrictMode>,
 );
